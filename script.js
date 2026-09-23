3858eb740af83e6ce06d4993b8d6d57641c7dfe5
CharlieMack97 <145165219+CharlieMack97@users.noreply.github.com>
2026-09-23 12:25:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    // Get the value from the input field
+    var inputValue = document.getElementById('myInput').value;
+    // Do something with the input value
+    console.log(inputValue);
+});
